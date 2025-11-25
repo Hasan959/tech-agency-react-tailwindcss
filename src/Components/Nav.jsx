4061b85fa565d2c61c6b22f33de9a6a1dfe590b4
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2025-11-25 23:45:03 +0600

    some change

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -47,6 +47,7 @@ const Nav = () => {
           ${menuOpen
              ? "opacity-100 translate-y-0 scale-100 pointer-events-auto"
              : "opacity-0 -translate-y-10 scale-y-0 pointer-events-none " }`}>
+
               <ul className="space-y-10">
                  <li>
                    <Link to="/" className='text-4xl lg:text-6xl font-bold' 
@@ -92,6 +93,8 @@ const Nav = () => {
 
                     </ul>
                 </li>
+
+
                  <li>
                   <Link to="/projects" className='text-4xl lg:text-6xl font-bold' 
                     onClick={()=> setMenuOpen(false) } >
@@ -158,26 +161,58 @@ const Nav = () => {
 export default Nav
 
 
+
+
+// import { Icon } from '@iconify/react';
 // import React, { useState } from 'react'
 // import { Link } from 'react-router'
 // const Nav = () => {
-// const [navBg, setNavBg ] = useState( false)
+//   const [menuOpen , setMenuOpen] =useState(false);
+// const [navBg, setNavBg ] = useState( false);
+// const [openDropdown, setOpenDropdown ] =useState("");
+// const toggleDropdown = (name) => {
+//   setOpenDropdown(openDropdown === name ?"" : name  )
+// }
 //   return (
 //    <>
-//    <div className={`${navBg ? "bg-white shadow-lg text-black" : "bg-transparent text-black"}`}>
+//    <div className={` h-[90px] flex justify-between items-center px-[2%] md:px-[8%] xl:px-[12%] fixed top-0 left-0 right-0 z-50 transition-all duration-500  ${navBg ? "bg-white shadow-lg text-black" : "bg-transparent text-white"}`}>
 
      
-//     <Link to="/" className={` ${navBg ? "text-black" : "text-white "  }`} >
+//     <Link to="/" className={` text-4xl font-semibold  ${navBg ? "text-black" : "text-white "  }`} >
 //     Naha <span className='text-primary' > R </span>
 //     </Link>
 
-//     <div className='cursor-pointer z-60 '>
-//       <span></span>
+//     <div className='cursor-pointer z-60' onClick={()=> setMenuOpen(!menuOpen) } >
+//       <span className= {`block w-10 h-0.5 transition-all duration-500 ${navBg ? "bg-black " : "bg-white"} ${menuOpen ? "rotate-45 translate-y-2 " : "" } `} >
+//       </span>
+//       <span className={`block w-10 h-0.5 mt-2.5 transition-all duration-500 ${navBg ? "bg-black" : "bg-white"}
+//        ${menuOpen ? "-rotate-45 -translate-y-1.5 ":""  } `}></span>
 //     </div>
-     
 //    </div>
-       
 
+
+//    <div className={`fixed top-0 left-0 w-full h-screen backdrop-blur-sm bg-black/90  px-[2%] md:px-[8%] xl:px-[12%] z-20 flex flex-col justify-center items-start transform transition-all duration-700 ease-[cubic-beizer(0,77,0,0.75,1)] origin-top ${menuOpen ?
+//      "opacity-100 translate-y-0 scale-100 pointer-events-auto text-white ":"opacity-0 -translate-y-10 scale-y-0 pointer-events-none text-orange-500"  }`}>
+//     <ul className='space-y-10'>
+//     <li>
+//       <Link to="/" className='text-4xl lg:text-6xl font-bold ' onClick={()=> setMenuOpen(false) }  >
+//       Home
+//       </Link>
+//     </li>
+
+//       <li>
+//         <Link to={"/about"} className='text-4xl lg:text-6xl font-bold' onClick={()=> setMenuOpen(false)} >
+//          About
+//         </Link>
+//       </li>
+//       <li className='relative' >
+//         <button onClick={()=> toggleDropdown("pages")} className='text-4xl'> Pages <Icon/>  </button>
+//       </li>
+
+    
+//    </ul>
+//    </div>
+     
 //    </>
 //   )
 // }
--- src/Pages/Faq.jsx
+++ src/Pages/Faq.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+//faq section
 const Faq = () => {
   return (
     <div>Faq</div>
