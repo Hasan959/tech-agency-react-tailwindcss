d0f2acdc19dc90c427473f885feba440881ccb33
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2026-01-07 21:58:46 +0600

    circle animation

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -544,6 +544,17 @@ const Index = () => {
               <div className='cat-img-circle img-circle--1  '></div>
               <div className='cat-img-circle img-circle--2'></div>
               <div className='cat-img-circle img-circle--3'></div>
+
+              <ul className='text-white grid grid-cols-2 gap-10 absolute right-0 bottom-60 social-icons'>
+                <li>
+                  <Link to='/' className='social-icon relative overflow-hidden p-10 text-xl font-bold h-[100px] rounded-full bg-black flex gap-8 items-center'>
+                   <Icon icon="ri:facebook-fill" width="44" height="44" className='text-black bg-white p-2 rounded-full '   />
+                  </Link>
+
+                </li>
+
+              </ul>
+
             </div>
           </div>
 
