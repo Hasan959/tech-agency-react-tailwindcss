f486a0177e268bd7a54f946a051789f31eb39615
Hasan Mahmud <57359405+Hasan959@users.noreply.github.com>
2026-01-18 16:38:07 +0000

    Add footer structure to Footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -17,6 +17,7 @@ const Footer = () => {
 
    
   return (
+    //footer start
     <footer className='bg-black text-gray-300 pt-10 pb-5 px-[2%] md:px-[8%] xl:px-[12%]'>
        <div className='max-w-7xl mx-auto border border-gray-600' >
         <div className='grid md:grid-cols-3 gap-10 py-15 '>
@@ -51,6 +52,7 @@ const Footer = () => {
         </div>
          <hr className='border-gray-700 mb-6' />
        </div>
+      
     </footer>
   )
 }
