0762081ec2eb4456628efbac9952882ae0e61ecc
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2026-01-21 23:05:56 +0600

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router"
 import Nav from "./Components/Nav"
 import Index from "./Pages/Index"
 import Footer from "./Components/Footer"
+import About from "./Pages/About"
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <BrowserRouter>
       <Nav/>
       <Routes>
-        <Route path="/" element={ <Index/> } />
+        <Route path="/" element={<Index/>} />
+        <Route path="/about" element={<About/>} />
       </Routes>
       <Footer/>
     
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import title_icon from "../assets/Images/title_icon.svg"
+import { Link } from 'react-router'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div className="section-banner h-[500px] bg-[#121212] px-[2%] md:px-[8%] xl:px-[12%] flex flex-col justify-end items-start text-white w-full">
+        <h2 className='text-5xl lg:text-7xl font-semibold w-full lg:max-w-2xl relative pb-3 ' >
+          About Us 
+          <span className='absolute hidden md:flex top-0 right-35 w-10 h-10' >
+            <img src={title_icon} alt="icon" className='w-full h-full'  />
+          </span>
+        </h2>
+        <p className='border-b-2 w-full pb-10 border-gray-300 ' >  
+          Creative studio at the intertsection of art ,designed technology.
+         </p>
+         <ul className='flex gap-8 py-5 ' >
+          <li>
+            <Link to={"/index"} className='font-normal text-white text-lg'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to={"/about"} className='font-normal text-white text-lg'>
+              About us
+            </Link>
+          </li>
+         </ul>
+      </div>
+    </>
   )
 }
 
