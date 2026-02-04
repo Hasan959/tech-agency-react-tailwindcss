0a9eb3e81a4bcbff1a60946cbf5e55a91c3ec12f
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2026-02-04 21:26:44 +0600

    teamdetails

--- src/Pages/TeamDetails.jsx
+++ src/Pages/TeamDetails.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React, { useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router'
+import { teamMembers } from './Team'
+import title_icon from "../assets/Images/title_icon.svg"
 
-const TeamDetails = () => {
+  const TeamDetails = () => {
+  const {id} =useParams()
+  const navigate = useNavigate() 
+
+  const member = teamMembers.find(m=> m.id ===id)
+
+  //export variable from team.jsx
+  useEffect(()=> {
+    if(!member) navigate("/team")
+  },[member,navigate])
+
+   if(!member) return null 
   return (
     <>
        <div className="section-banner h-[500px] bg-[#121212] px-[2%] md:px-[8%] xl:px-[12%] flex flex-col justify-end items-start text-white w-full">
-        <h2 className='text-5xl lg:text-7xl font-semibold w-full lg:max-w-2xl relative pb-3 ' >
-          Our Team 
+        <h2 className='text-5xl lg:text-7xl font-semibold w-full lg:max-w-2xl relative pb-3'>
+          {member.name}
         </h2>
         <p className='border-b-2 w-full pb-10 border-gray-300 ' >  
           Meeting Our Creativity company family.
