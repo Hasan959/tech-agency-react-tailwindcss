66242b898adc4d449243a1928a427d9ad066099e
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2026-02-15 13:09:00 +0600

    psudo css

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -10,6 +10,7 @@ import sericon3 from '../assets/Images/serv-icon6.png'
 const Services = () => {
   return (
     <>
+       {/* div start */}
       <div className="section-banner h-[500px] bg-[#121212] px-[2%] md:px-[8%] xl:px-[12%] flex flex-col justify-end items-start text-white w-full">
         <h2 className='text-5xl lg:text-7xl font-semibold w-full lg:max-w-2xl relative pb-3 ' >
           Our service
@@ -33,7 +34,9 @@ const Services = () => {
           </li>
          </ul>
       </div>
-
+      {/* div end */}
+      
+      {/* div start */}
       <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-14 py-[8%] px-[2%] md:px-[8%] xl:px-[12%] overflow-x-hidden'> 
         <div className='ser-item border border-gray-50/20 text-center rounded-sm relative'>
         <div className='image bg-primary mx-auto w-30 h-30 md:w-40 md:h-40 p-8 rounded-full transform -translate-y-8 md:-translate-y-14'>
@@ -61,35 +64,62 @@ const Services = () => {
           </div>
         </div>
 
-        <div className='ser-item border border-gray-50/20 text-center rounded-sm relative'>
-        <div className='image bg-primary mx-auto w-30 h-30 md:w-40 md:h-40 p-8 rounded-full transform -translate-y-8 md:-translate-y-14'>
-          <img src={sericon2} alt="service-icon" />
-        </div>
-          <div className='service-info text-white p-8 pt-0 ' >
-             <h4 className='text-4xl font-semibold pb-4'> Brand Creation </h4>
+         <div className='ser-item border border-gray-50/20 text-center rounded-sm relative'>
+           <div className='image bg-primary mx-auto w-30 h-30 md:w-40 md:h-40 p-8 rounded-full transform -translate-y-8 md:-translate-y-14'>
+           <img src={sericon2} alt="service-icon" />
+         </div>
+           <div className='service-info text-white p-8 pt-0 ' >
+             <h4 className='text-4xl font-semibold pb-4'> Web campaigns </h4>
              <ul className='space-y-5 text-gray-300'>
                <li>
-                 <span> Brand Strategy  </span>
+                 <span> Creative Direction  </span>
                </li>
                <li>
-                 <span> Visual Identity   </span>
+                 <span> concepts   </span>
                </li>
                <li>
-                 <span> Naming   </span>
+                 <span> copyrights  </span>
                </li>
                <li>
-                 <span> Toolskits </span>
+                 <span> social media </span>
                </li>
                <li>
-                 <span> Guidelines   </span>
+                 <span> Creative Strategy    </span>
                </li>
-                <li>
-                 <span> Guidelines   </span>
-               </li>
+                
              </ul>
+           </div>
+         </div>
+
+        <div className='ser-item border border-gray-50/20 text-center rounded-sm relative'>
+         <div className='image bg-primary mx-auto w-30 h-30 md:w-40 md:h-40 p-8 rounded-full transform -translate-y-8 md:-translate-y-14'>
+           <img src={sericon3} alt="service-icon" />
+         </div>
+           <div className='service-info text-white p-8 pt-0 ' >
+              <h4 className='text-4xl font-semibold pb-4'> UI/UX Design</h4>
+               <ul className='space-y-5 text-gray-300'>
+                <li>
+                 <span> Native and web apps  </span>
+                </li>
+                <li>
+                 <span> campaign /brand sites   </span>
+               </li>
+               <li>
+                 <span> Machine Learning/ AI   </span>
+               </li>
+               <li>
+                 <span>Brand Design </span>
+               </li>
+               <li>
+                 <span> Production Animation Design    </span>
+               </li>
+                
+             </ul>
+           </div>
           </div>
-        </div>
+
       </div>
+       {/* div end */}
     </>
   )
 }
